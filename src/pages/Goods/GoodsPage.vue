df455793266ee3c18e3a30c7c14362028ffbb194
caferoom <368974390@qq.com>
2024-09-20 08:55:19 +0800

    111

--- src/pages/Goods/GoodsPage.vue
+++ src/pages/Goods/GoodsPage.vue
@@ -6,21 +6,11 @@
             </router-link>
         </div>
         <div class="content-main">
-            <!-- <div class="block">
-				<span class="wrapper">
-					<el-button :plain="true" type="primary" :class="activeClass == 1 ? 'active' : '' "
-							   @click="sortOrder(1)" size="small">按销量排序</el-button>
-					<el-button :plain="true" type="primary" :class="activeClass == 2 ? 'active' : '' "
-							   @click="sortOrder(2)" size="small">按售价排序</el-button>
-					<el-button :plain="true" type="primary" :class="activeClass == 3 ? 'active' : '' "
-							   @click="sortOrder(3)" size="small">按库存排序</el-button>
-				</span>
-            </div> -->
-            <el-tabs v-model="activeName" @tab-click="handleClick">
-                <el-tab-pane label="全部商品" name="first"/>
-                <el-tab-pane label="出售中" name="second"/>
-                <el-tab-pane label="已售完" name="third"/>
-                <el-tab-pane label="已下架" name="fourth"/>
+            <el-tabs v-model="activeTab" @tab-click="handleClick">
+                <el-tab-pane label="全部商品" :name="Tabs.All"/>
+                <el-tab-pane label="出售中" :name="Tabs.OnSale"/>
+                <el-tab-pane label="已售完" :name="Tabs.SellOut"/>
+                <el-tab-pane label="已下架" :name="Tabs.Unlisted"/>
             </el-tabs>
             <div class="filter-box">
                 <el-form :inline="true" :model="filterForm" class="form-inline">
@@ -28,7 +18,7 @@
                         <el-input size="small" v-model="filterForm.name" placeholder="商品名称"></el-input>
                     </el-form-item>
                     <el-form-item>
-                        <el-button type="primary" size="small" @click="onSubmitFilter">查询</el-button>
+                        <el-button type="primary" size="small" @click="onSearch">查询</el-button>
                         <el-button size="small" @click="clear">清空</el-button>
                     </el-form-item>
                 </el-form>
@@ -44,7 +34,7 @@
                                                 v-model="scope.row.goods_sn" placeholder="商品SKU"></el-input>
                                 </template>
                             </el-table-column>
-                            <el-table-column prop="goods_aka" label="快递单上的简称" width="160">
+                            <el-table-column prop="goods_name" label="快递单上的简称" width="160">
                                 <template slot-scope="scope">
                                     <el-input size="mini" v-model="scope.row.goods_name"
                                                 placeholder="简称"></el-input>
@@ -151,217 +141,32 @@
 </template>
 
 <script>
+    const Tabs = {
+        All: "all",
+        OnSale: "onSale",
+        SellOut: "sellout",
+        Unlisted: "unlisted",
+    };
+
     export default {
         data() {
             return {
+                // tab切换
+                Tabs,
+                activeTab: Tabs.All,
+
                 page: 1,
-				size:10,
+				size: 10,
                 total: 0,
                 filterForm: {
                     name: ''
                 },
                 tableData: [],
-                activeName: 'second',
-                pIndex: 0,
-                num: 0,
-                activeClass: 0,
-                expand:true,
             }
         },
         methods: {
-            stockSyc(){
-                this.$confirm('确定要同步库存?', '提示', {
-                    confirmButtonText: '确定',
-                    cancelButtonText: '取消',
-                    type: 'warning'
-                }).then(() => {
-                    this.axios.post('crontab/updateStockByHand').then((response) => {
-                        if (response.success) {
-                            this.$message({
-                                type: 'success',
-                                message: '同步成功'
-                            });
-                            this.getOnSaleList();
-                        }
-                    })
-                }).catch(() => {
-
-                });
-            },
-            updateGoodsNumber(){
-                this.axios.post('goods/updateGoodsNumber').then((response) => {
-                    if (response.success) {
-                        this.$message({
-                            type: 'success',
-                            message: '同步成功2/2，完成'
-                        });
-                    }
-                })
-            },
-            specSave(index,row){
-                if(row.goods_name == '' || row.value == '' || row.cost == '' || row.retail_price == '' || row.goods_weight == ''){
-                    this.$message({
-                        type: 'error',
-                        message: '值不能为空!'
-                    });
-                    return false;
-                }
-                this.axios.post('goods/updatePrice', row).then((response) => {
-                    if (response.success) {
-                        this.$message({
-                            type: 'success',
-                            message: '修改成功!'
-                        });
-                    }
-					else {
-                        
-						this.$message({
-						    type: 'error',
-						    message: response.message.message
-						});
-					}
-                })
-               
-            },
-            checkSkuOnly(index,row){
-                console.log(index);
-                console.log(row);
-                if(row.goods_sn == ''){
-                    this.$message({
-                        type: 'error',
-                        message: 'SKU不能为空'
-                    })
-                    return false;
-                }
-                this.axios.post('goods/checkSku', {info: row}).then((response) => {
-                    if (response.success) {
-                        this.$message({
-                            type: 'success',
-                            message: '该SKU可以用！'
-                        })
-                    }
-                    else{
-                        this.$message({
-                            type: 'error',
-                            message: '该SKU已存在！'
-                        })
-                    }
-                })
-            },
-            // do not used and what's shot_name
-            submitName(index, row){
-                this.axios.post('goods/updateShortName', { id: row.id,short_name:row.short_name }).then((response) => {
-                    if (response.success) {
-                        this.$message({
-                            type: 'success',
-                            message: '修改成功!'
-                        });
-                    }
-                })
-            },
-            submitSort(index, row){
-                this.axios.post('goods/updateSort', { id: row.id,sort:row.sort_order }).then((response) => {})
-            },
-            handleClick(tab, event) {
-                let pindex = tab._data.index;
-				this.page = 1;
-                this.activeClass = 0;
-                if (pindex == 0) {
-                    this.getList();
-                    this.pIndex = 0;
-                }
-                else if (pindex == 1) {
-                    this.getOnSaleList();
-                    this.pIndex = 1;
-                }
-                else if (pindex == 2) {
-                    this.getOutList();
-                    this.pIndex = 2;
-                }
-                // else if (pindex == 3) {
-                //     this.getDropList();
-                //     this.pIndex = 3;
-                // }
-            },
-            handlePageChange(val) {
-                this.page = val;
-                let nIndex = this.pIndex;
-                if (nIndex == 0) {
-                    this.getList();
-                }
-                else if (nIndex == 1) {
-                    this.getOnSaleList();
-                }
-                else if (nIndex == 2) {
-                    this.getOutList();
-                }
-                else if (nIndex == 3) {
-                    this.getDropList();
-                }
-                // else if (nIndex == 4) {
-                //     this.sortOrder(this.num);
-                // }
-            },
-            handleRowEdit(index, row) {
-                this.$router.push({name: 'goods_add', query: {id: row.id}})
-            },
-            handleRowDelete(index, row) {
-
-                this.$confirm('确定要删除?', '提示', {
-                    confirmButtonText: '确定',
-                    cancelButtonText: '取消',
-                    type: 'warning'
-                }).then(() => {
-                let that = this;
-                that.axios.post('goods/destory', {id: row.id}).then((response) => {
-                    if (response.success) {
-                        that.$message({
-                            type: 'success',
-                            message: '删除成功!'
-                        });
-                        let pIndex = localStorage.getItem('pIndex');
-                        console.log(pIndex);
-                        if (pIndex == 0) {
-                            that.getList();
-                        }
-                        else if (pIndex == 1) {
-                            that.getOnSaleList();
-                        }
-                        else if (pIndex == 2) {
-                            that.getOutList();
-                        }
-                        else if (pIndex == 3) {
-                            that.getDropList();
-                        }
-                    }
-                })
-                }).catch(() => {
-//                    this.$message({
-//                        type: 'info',
-//                        message: '已取消删除'
-//                    });
-                });
-            },
-            onSubmitFilter() {
-                this.page = 1;
-                this.getList();
-            },
-            clear(){
-                this.axios.get('goods', {
-                    params: {
-                        page: this.page,
-                        name: ''
-                    }
-                }).then((response) => {
-                    if (response.success) {
-                        this.tableData = response.data.data
-                        this.page = response.data.currentPage
-                        this.total = response.data.count
-                    }
-                })
-            },
-            getList() {
-                this.axios.get('goods', {
+            getAll() {
+                this.axios.get('goods/all', {
                     params: {
                         page: this.page,
 						size: this.size,
@@ -417,24 +222,133 @@
                     }
                 })
             },
-            // sortOrder(num) {
-            //     this.num = num;
-            //     this.pIndex = 4;
-            //     this.activeClass = num;
-            //     this.axios.get('goods/sort', {
-            //         params: {
-            //             page: this.page,
-			// 			size: this.size,
-            //             index: num
-            //         }
-            //     }).then((response) => {
-            //         if (response.success) {
-            //             this.tableData = response.data.data;
-            //             this.page = response.data.currentPage;
-            //             this.total = response.data.count;
-            //         }
-            //     })
-            // },
+            getTableData() {
+                if (this.activeTab === Tabs.All) {
+                    this.getAll();
+                } else if (this.activeTab === Tabs.OnSale) {
+                    this.getOnSaleList();
+                } else if (this.activeTab === Tabs.SellOut) {
+                    this.getOutList();
+                } else if (this.activeTab === Tabs.Unlisted) {
+                    this.getDropList();
+                }
+            },
+            specSave(index,row){
+                if(row.goods_name == '' || row.value == '' || row.cost == '' || row.retail_price == '' || row.goods_weight == ''){
+                    this.$message({
+                        type: 'error',
+                        message: '值不能为空!'
+                    });
+                    return false;
+                }
+                this.axios.post('goods/updatePrice', row).then((response) => {
+                    if (response.success) {
+                        this.$message({
+                            type: 'success',
+                            message: '修改成功!'
+                        });
+                    }
+					else {
+                        
+						this.$message({
+						    type: 'error',
+						    message: response.message.message
+						});
+					}
+                })
+               
+            },
+            checkSkuOnly(index,row){
+                console.log(index);
+                console.log(row);
+                if(row.goods_sn == ''){
+                    this.$message({
+                        type: 'error',
+                        message: 'SKU不能为空'
+                    })
+                    return false;
+                }
+                this.axios.post('goods/checkSku', {info: row}).then((response) => {
+                    if (response.success) {
+                        this.$message({
+                            type: 'success',
+                            message: '该SKU可以用！'
+                        })
+                    }
+                    else{
+                        this.$message({
+                            type: 'error',
+                            message: '该SKU已存在！'
+                        })
+                    }
+                })
+            },
+            submitSort(index, row){
+                this.axios.post('goods/updateSort', { id: row.id,sort:row.sort_order }).then((response) => {})
+            },
+            handleClick(tab, event) {
+                this.tableData = [];
+				this.page = 1;
+                this.getTableData();
+            },
+            handlePageChange(val) {
+                this.page = val;
+                this.getTableData();
+            },
+            handleRowEdit(index, row) {
+                this.$router.push({name: 'goods_add', query: {id: row.id}})
+            },
+            handleRowDelete(index, row) {
+                this.$confirm('确定要删除?', '提示', {
+                    confirmButtonText: '确定',
+                    cancelButtonText: '取消',
+                    type: 'warning'
+                }).then(() => {
+                    this.axios.post('goods/destory', {id: row.id}).then((response) => {
+                    if (response.success) {
+                        this.$message({
+                            type: 'success',
+                            message: '删除成功!'
+                        });
+                        if (this.activeTab === Tabs.All) {
+                            this.getAll();
+                        } else if (this.activeTab === Tabs.OnSale) {
+                            this.getOnSaleList();
+                        } else if (this.activeTab === Tabs.SellOut) {
+                            this.getOutList();
+                        } else if (this.activeTab === Tabs.Unlisted) {
+                            this.getDropList();
+                        }
+                    }
+                })
+                }).catch(() => {
+//                    this.$message({
+//                        type: 'info',
+//                        message: '已取消删除'
+//                    });
+                });
+            },
+            onSearch() {
+                this.page = 1;
+                this.activeTab = Tabs.All;
+                this.getAll();
+            },
+            clear(){
+                this.page = 1;
+                this.activeTab = Tabs.All;
+                this.axios.get('goods/all', {
+                    params: {
+                        page: this.page,
+                        name: ''
+                    }
+                }).then((response) => {
+                    if (response.success) {
+                        this.tableData = response.data.data
+                        this.page = response.data.currentPage
+                        this.total = response.data.count
+                    }
+                })
+            },
             changeStatus($event, para) {
                 this.axios.get('goods/saleStatus', {
                     params: {
@@ -464,10 +378,9 @@
             }
         },
         mounted() {
-            this.getOnSaleList();
+            this.getTableData();
         }
     }
-
 </script>
 
 <style scoped>
